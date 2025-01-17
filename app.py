dfe1bcaeec5fd53bd97820bc36659fe262f46414
Namir50 <namirsayyed50@gmail.com>
2025-01-17 14:48:16 +0530

    New

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
-from flask_scss import Scss
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def main():
+    return render_template('index.html')
 
-if __name__ in "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
+    
