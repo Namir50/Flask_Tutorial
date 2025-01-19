78672c3575209e105fb03b40d8bc5cff6662f8ab
Namir50 <namirsayyed50@gmail.com>
2025-01-19 14:13:21 +0530

    Routes

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    name = "Namir"
+    l1 = ["Namir","Ibaad","Armaan"]
+    return render_template('index.html',name = name,l1 = l1)
+    
+@app.route("/friend")
+def friend():
+    return "This is friends page"
+
+@app.route("/friends/<name>")
+def friends(name):
+    return render_template('index2.html',name = name)
 
 if __name__ == '__main__':
     app.run(debug=True)
