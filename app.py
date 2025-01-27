4cce0815361c370647205bedf04b9b16cf043a53
Namir50 <namirsayyed50@gmail.com>
2025-01-27 14:50:06 +0530

    Routes

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def friend():
 def friends(name):
     return render_template('index2.html',name = name)
 
+@app.route("/hello")
+def hello():
+    l2 = ["My","Name","is","Namir"]
+    return render_template('index3.html',l2 = l2)
+        
+
 if __name__ == '__main__':
     app.run(debug=True)
     
