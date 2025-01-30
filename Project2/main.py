ad992c586f2f00aedfe04a7807cb224c20f30761
Namir50 <namirsayyed50@gmail.com>
2025-01-30 14:02:44 +0530

    GET POST

--- Project2/main.py
+++ Project2/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,8 +10,12 @@ def home():
 def friends():
     return render_template('index2.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        password = request.form.get('password')
+        return f"Received Name: {name}, Password: {password}"
     return render_template('index3.html')
 
 if __name__ == '__main__':
