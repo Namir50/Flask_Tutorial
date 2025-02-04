6f97b83ba2dfb725eca636ebc1c21b4cbe98dea3
Namir50 <namirsayyed50@gmail.com>
2025-02-04 14:03:20 +0530

    New

--- Project2/main.py
+++ Project2/main.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+db = SQLAlchemy(app)
+
+class DBhandler(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200))
+    password = db.Column(db.Integer)
+    
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -19,4 +27,5 @@ def about():
     return render_template('index3.html',name = name, password = password)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
